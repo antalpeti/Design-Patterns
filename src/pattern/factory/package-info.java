3525a7a844b8eada335c0ddb7b0bdc5e318585a2
Peti <antalpeti@users.noreply.github.com>
2015-02-02 23:10:45 +0100

    Fixed formatting.

--- src/pattern/factory/package-info.java
+++ src/pattern/factory/package-info.java
@@ -1,4 +1,5 @@
 /**
+ * <h1>Design Pattern - Factory Pattern</h1>
  * <p>
  * Factory pattern is one of most used design pattern in Java. This type of design pattern comes
  * under creational pattern as this pattern provides one of the best ways to create an object.
@@ -9,11 +10,12 @@
  * to newly created object using a common interface.
  * </p>
  *
+ * <h2>Implementation</h2>
  * <p>
- * Implementation We're going to create a Shape interface and concrete classes implementing the
- * Shape interface. A factory class ShapeFactory is defined as a next step. FactoryPatternDemo, our
- * demo class will use ShapeFactory to get a Shape object. It will pass information (CIRCLE /
- * RECTANGLE / SQUARE) to ShapeFactory to get the type of object it needs.
+ * We're going to create a Shape interface and concrete classes implementing the Shape interface. A
+ * factory class ShapeFactory is defined as a next step. FactoryPatternDemo, our demo class will use
+ * ShapeFactory to get a Shape object. It will pass information (CIRCLE / RECTANGLE / SQUARE) to
+ * ShapeFactory to get the type of object it needs.
  * </p>
  * <img src="./factory_pattern_uml_diagram.jpg" alt="Factory Pattern UML Diagram" width="100%">
  */
