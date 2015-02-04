affe678380a6722e99ec80ec5c70a467bbacb6b0
Peti <antalpeti@users.noreply.github.com>
2015-02-04 09:49:43 +0100

    Added the Adapter Pattern.

---
+++ src/pattern/adapter/VlcPlayer.java
@@ -0,0 +1,13 @@
+package pattern.adapter;
+
+public class VlcPlayer implements AdvancedMediaPlayer {
+  @Override
+  public void playVlc(String fileName) {
+    System.out.println("Playing " + AudioType.VLC.getType() + " file. Name: " + fileName);
+  }
+
+  @Override
+  public void playMp4(String fileName) {
+    // do nothing
+  }
+}
