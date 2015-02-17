6a5eb61ddadd6173bfe000a5045783e8846129e5
Peti <antalpeti@users.noreply.github.com>
2015-02-17 17:23:29 +0100

    Added the Visitor Pattern.

---
+++ src/pattern/visitor/Mouse.java
@@ -0,0 +1,9 @@
+package pattern.visitor;
+
+public class Mouse implements ComputerPart {
+
+  @Override
+  public void accept(ComputerPartVisitor computerPartVisitor) {
+    computerPartVisitor.visit(this);
+  }
+}
